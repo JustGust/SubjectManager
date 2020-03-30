d5b2d4d10270e7ff21ebcd2854a683106662aa7c
Gustavo Alomia <gustavo@MacBook-Pro-de-Gustavo.local>
2020-03-30 12:22:21 -0500

    firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCuL8V6Ua_keVHJHVvnjGUrdjUEUZZiXQk",
+    authDomain: "subjectmanager-b8261.firebaseapp.com",
+    databaseURL: "https://subjectmanager-b8261.firebaseio.com",
+    projectId: "subjectmanager-b8261",
+    storageBucket: "subjectmanager-b8261.appspot.com",
+    messagingSenderId: "425462092627",
+    appId: "1:425462092627:web:456b73607d25f59b4747a6",
+    measurementId: "G-QET683LMPX"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCuL8V6Ua_keVHJHVvnjGUrdjUEUZZiXQk",
+    authDomain: "subjectmanager-b8261.firebaseapp.com",
+    databaseURL: "https://subjectmanager-b8261.firebaseio.com",
+    projectId: "subjectmanager-b8261",
+    storageBucket: "subjectmanager-b8261.appspot.com",
+    messagingSenderId: "425462092627",
+    appId: "1:425462092627:web:456b73607d25f59b4747a6",
+    measurementId: "G-QET683LMPX"
+  }
+
 };
 
 /*
